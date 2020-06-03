42c55a80d05fde34c5f64add1b8b11d3c39c63c6
Matt Long <mdlong34@gmail.com>
2020-06-03 21:06:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(string)
-  array = [string]
-  array.each do |element|
-    element.reverse!
-  end 
-  new_string = array.to_s
-  new_string
-end 
+  string.split.collect {|word| word.reverse}.join(" ")
+end
